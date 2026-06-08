6afed4fa3681453bec5eed1d7654546d494eb208
Sara Jiménez Torres <sarajimeneztorres90@gmail.com>
2026-06-08 17:18:06 +0200

    Update conexion.py

--- database/conexion.py
+++ database/conexion.py
@@ -4,8 +4,8 @@ import mysql.connector
 def conectar_bd():
     conn = mysql.connector.connect(
         host="localhost",
-        user="TU_USUARIO",
-        password="TU_PASSWORD",
+        user="usuario_mysql",
+        password="password_mysql",
         database="hotel_revenue_db"
     )
     return conn
